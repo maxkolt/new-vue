44c1a07ed929d430150f63aa97c2eb6f885f346a
Администратор <12345kolt@gmail.com>
2021-03-28 13:02:05 +0300

    Отправка запросов

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>{{ count }}</h3>
+    <h3>Ответ от сервака: {{ serverData }}</h3>
+
+    <button @click="clickCounter"><b>sdfdsf</b></button>
+    <button @click="getFromServer">From server</button>
+    <button @click="getFromServer">From server2</button>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
+
   name: 'App',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      count: 0,
+      serverData: null,
+      serverData2: null
+    }
+  },
+  methods: {
+    clickCounter() {
+      this.count = this.count + 1;
+    },
+    getFromServer() {
+      axios
+          .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+          .then(response => (
+              this.serverData = response.data.bpi.USD.code
+          ));
+    },
+  getFromDataServer(){
+      axios
+          .get('https://www.foaas.com/cup/:Maxim')
+          .then(response => (
+              this.serverData = response.data.bpi.USD.code
+          ));
+  }
+
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
