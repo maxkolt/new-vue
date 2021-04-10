0d4b9d9ca4a012de8edc7861737047445a0fc28f
Администратор <12345kolt@gmail.com>
2021-04-11 00:21:19 +0300

    resume на vue, начальный этап

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="hello">
+    <h3>{{ temperstura }}</h3>
+    <h5>{{ veter }}</h5>
+    <h5>{{davlenie}}</h5>
     <button class="button" @click="onClick">Прогноз погоды</button>
   </div>
 </template>
@@ -9,14 +12,16 @@ export default {
   name: 'HelloWorld',
   data(){
     return{
-      count: 23
+      temperstura: 23 + 'C',
+      veter: 23 + 'км.ч',
+      davlenie: 48 + '%'
 
     }
 
   },
   methods: {
     onClick() {
-
+      this.;
     }
   }
 }
